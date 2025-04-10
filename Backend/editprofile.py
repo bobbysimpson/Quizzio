4df2d021e0866a92f5e81427be0db17aaff9f400
Rory Martin <rorymartin2005@gmail.com>
2025-04-10 20:37:57 +0100

    Fixed a bug in edit profile

--- Backend/editprofile.py
+++ Backend/editprofile.py
@@ -12,10 +12,10 @@ editprofile = Blueprint('editprofile', __name__)
 def profile():
     session['_flashes'].clear() # note this breaks all flashed messages on this page, need to fix this later
     supabase = current_app.config["SUPABASE_CLIENT"]
-    response = supabase.table("users").select("email").eq("username", session["username"]).execute()
+    response = supabase.table("users").select("email").eq("username", current_user.username).execute()
     email = response.data[0]
     email = email.get("email")
-    return render_template('profile.html', username=session["username"], email=email)
+    return render_template('profile.html', username=current_user.username, email=email)
 
 @editprofile.route('/edit', methods = ['GET', 'POST'])
 def edit():
@@ -41,7 +41,7 @@ def edit():
                 updateData['email'] = newEmail
         if len(newPassword) != 0:
             updateData['password'] = generate_password_hash(newPassword)
-        response = supabase.table("users").update(updateData).eq("username", session["username"]).execute()
+        response = supabase.table("users").update(updateData).eq("username", current_user.username).execute()
        # print(response.data)
         if not response.data or len(response.data) == 0:
             flash("Error: Nothing happened", "error")
@@ -50,9 +50,9 @@ def edit():
             flash("User data saved successfully", "success") # need to flash a message here
             print("Did work")
             if len(newUsername) != 0:
-                session["username"] = newUsername
-    response = supabase.table("users").select("email").eq("username", session["username"]).execute()
+                current_user.username = newUsername
+    response = supabase.table("users").select("email").eq("username", current_user.username).execute()
     email = response.data[0]
     email = email.get("email")
-    return render_template('profile.html', username=session["username"], email=email)
+    return render_template('profile.html', username=current_user.username, email=email)
     
